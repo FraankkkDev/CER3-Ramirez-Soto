57d1a9d2c0503a17f8273160efbe55bfa44350ce
Frank Soto <fsotoa@usm.cl>
2025-11-30 16:16:26 -0300

    Creación de urls y serializers con su contenido

--- EcoLibrary/core/admin.py
+++ EcoLibrary/core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Libro, Favorito
 
-# Register your models here.
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'categoria', 'anio_publicacion')
+    search_fields = ('titulo', 'autor')
+    list_filter = ('categoria', 'anio_publicacion')
+
+@admin.register(Favorito)
+class FavoritoAdmin(admin.ModelAdmin):
+    list_display = ('libro', 'usuario', 'fecha_agregado')
+    search_fields = ('libro__titulo', 'usuario__username')
+    list_filter = ('fecha_agregado',)
+
